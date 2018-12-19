a205a81b8e380180a88072fd51780f638cb4c6a6
Dave Searle <dave@dave.ninja>
2018-12-19 10:37:17 +0000

    store series data in StockPriceChart component state

--- src/react-app/src/components/StockPriceChart.js
+++ src/react-app/src/components/StockPriceChart.js
@@ -20,12 +20,15 @@ const mapDispatchToProps = (dispatch) => ({
 
 class StockPriceChart extends Component {
 
+    state = {
+        series: []
+    }
+
     getData = (symbol, callback) => {
         axios.get('http://localhost:5000/prices/' + symbol)
             .then(response => {
     
                 var prices = Array.from(response.data.map(suggestion => suggestion.Close));
-
                 callback(prices);
             })
             .catch(error => { console.log(error); })
@@ -44,45 +47,56 @@ class StockPriceChart extends Component {
         return -1;
     }
 
-    removeSeries(symbol) {
-        let chart = this.refs.chart.getChart();
-        var seriesIndex = this.findSeriesIndex(symbol);
-        if(seriesIndex > -1)
-            chart.series[seriesIndex].remove();
-    }
-
-    addSeries(symbol, prices) {
-        let chart = this.refs.chart.getChart();
-        chart.addSeries({
-            name: symbol,
-            data: prices
-        });
-    }
-
     componentDidUpdate(prevProps) {
 
         if (this.props.symbols !== prevProps.symbols) {
 
-            prevProps.symbols.forEach((symbol) => {
-                if(this.props.symbols.indexOf(symbol) === -1) {
-                    this.removeSeries(symbol);
-                }
-            });
-       
+            // for new symbols, get the price data and add to the component's state
             this.props.symbols.forEach((symbol) => {
-                var seriesIndex = this.findSeriesIndex(symbol);
-                if(seriesIndex === -1) {
+                if(prevProps.symbols.indexOf(symbol) === -1) {
                     this.getData(symbol, (prices) => {
-                        this.addSeries(symbol, prices);
+                        let newSeries = { name: symbol, data: prices }
+                        this.setState(prevState => ({
+                            series: [...prevState.series, newSeries]
+                        }))
                     });
                 }
             });
+
+            // for deleted symbols, remove them from the state
+            prevProps.symbols.forEach((symbol) => {
+                if(this.props.symbols.indexOf(symbol) === -1) {
+                    this.setState(prevState => ({
+                        series: prevState.series.filter((item, _) => item.name !== symbol)
+                    }))
+                }
+            });
         }
+
+        this.renderSeries(this.state.series);
+    }
+
+    renderSeries(series) {
+
+        let chart = this.refs.chart.getChart();
+        let symbols = Array.from(series.map((item) => item.name));
+
+        // for each series in the state, make sure it's been added to the chart
+        series.forEach(item => {
+            if(this.findSeriesIndex(item.name) === -1)
+                chart.addSeries(item)
+        })
+
+        // remove series from the chart that arent in the state
+        chart.series.forEach(item => {
+            if(symbols.indexOf(item.name) === -1)
+                item.remove();
+        })
     }
 
     render() {
         const options = getOptions();
-
+        
         return (
             <ReactHighcharts 
                 ref="chart"
